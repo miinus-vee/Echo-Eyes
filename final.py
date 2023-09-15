4d8277ea21c40172f4501854c0ac6bb5f6f16ac3
Manasvi <91024452+miinus-vee@users.noreply.github.com>
2023-09-15 17:18:24 +0530

    Updated final.py

--- final.py
+++ final.py
@@ -5,23 +5,36 @@ import pyttsx3
 import pyautogui
 import time
 
-sound = pyttsx3.init()  # tts initiated
-rate = sound.getProperty("rate")  # rate determined
 
-new_rate = rate - 50  # rate decreased by 50 units
+
+sound = pyttsx3.init()
+rate = sound.getProperty("rate")  # Get the current speech rate
+
+# Decrease the rate by a specific amount, e.g., by 50 units
+new_rate = rate - 50
 sound.setProperty("rate", new_rate)
 
-
+# Store the previous cursor position
 previous_cursor_position = pyautogui.position()
+
+# Variable to control the main loop
+running = False
+
+# Store the previous cursor position
+previous_cursor_position = pyautogui.position()
+
+# Variable to control the main loop
 running = False
 
 
 def take_screenshot():
-    screenshot = pyautogui.screenshot()  # Entire Screen Screenshot
+    # Capture a screenshot of the entire screen
+    screenshot = pyautogui.screenshot()
     cursor_position = pyautogui.position()
 
+    # Determine the region of interest around the cursor position
     x, y = cursor_position
-    region = (x, y - 20, x + 250, y + 20)  # region determination
+    region = (x, y - 20, x + 250, y + 20)
     region_image = screenshot.crop(region)
     region_image_gray = region_image.convert("L")
     extracted_text = pytesseract.image_to_string(region_image_gray)
@@ -32,10 +45,10 @@ def detect_screen_change(previous_text):
     global previous_cursor_position
     current_cursor_position = pyautogui.position()
 
-    if (
-        current_cursor_position != previous_cursor_position
-    ):  # Check if the cursor has moved from its previous position
-        new_text = take_screenshot()  # Capture a new screenshot and perform OCR
+    # Check if the cursor has moved from its previous position
+    if current_cursor_position != previous_cursor_position:
+        # Capture a new screenshot and perform OCR
+        new_text = take_screenshot()
 
         # Compare the text extracted from the current and previous screenshots
         if new_text != previous_text:
@@ -56,10 +69,9 @@ def main_loop():
     initial_text = take_screenshot()
 
     while running:
-        initial_text = detect_screen_change(
-            initial_text
-        )  # Check for cursor movement and screen change
-        time.sleep(1)
+        # Check for cursor movement and screen change
+        initial_text = detect_screen_change(initial_text)
+        time.sleep(0.5)
 
 
 def start_main_loop():
@@ -76,18 +88,25 @@ def stop_main_loop():
 class MyFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(650, 650))
-        icon = wx.Icon("logo.png", wx.BITMAP_TYPE_PNG)
+
+        # Set the favicon (replace 'logo.png' with the actual path)
+        icon = wx.Icon("Logo.png", wx.BITMAP_TYPE_PNG)
         self.SetIcon(icon)
 
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
-        self.logo_bitmap = wx.Bitmap("logo.png", wx.BITMAP_TYPE_PNG)
+
+        # Load the logo image
+        self.logo_bitmap = wx.Bitmap("Logo.png", wx.BITMAP_TYPE_PNG)
+
+        # Create a StaticBitmap widget to display the logo
         self.logo_staticbitmap = wx.StaticBitmap(
             panel, wx.ID_ANY, self.logo_bitmap, wx.DefaultPosition, wx.DefaultSize
         )
         vbox.Add(self.logo_staticbitmap, flag=wx.ALIGN_CENTER | wx.ALL, border=10)
 
-        button_size = wx.Size(200, 60)  # Increase in button size
+        # Increase the size of the buttons
+        button_size = wx.Size(200, 60)
         launch_button = wx.Button(panel, label="Launch", size=button_size)
         quit_button = wx.Button(panel, label="Quit", size=button_size)
         vbox.Add(launch_button, flag=wx.ALIGN_CENTER | wx.ALL, border=10)
